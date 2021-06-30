edf648de142c74f8885c3035a938dfa7ac0f577d
Fabian Boemer <fabian.boemer@intel.com>
2021-06-30 09:59:16 -0700

    WIP debugging

--- hexl/eltwise/eltwise-dot-mod.cpp
+++ hexl/eltwise/eltwise-dot-mod.cpp
@@ -74,14 +74,14 @@ void EltwiseDotMod(uint64_t* result, const uint64_t* operand1,
   HEXL_CHECK_BOUNDS(operand4, n, modulus,
                     "pre-dot value in operand1 exceeds bound " << modulus);
 
-#ifdef HEXL_HAS_AVX512DQ
-  if (has_avx512dq && modulus < (1ULL << 50)) {
-    LOG(INFO) << "EltwiseDotModAVX512";
-    EltwiseDotModAVX512(result, operand1, operand2, operand3, operand4, n,
-                        modulus);
-    return;
-  }
-#endif
+  // #ifdef HEXL_HAS_AVX512DQ
+  //   if (has_avx512dq && modulus < (1ULL << 50)) {
+  //     LOG(INFO) << "EltwiseDotModAVX512";
+  //     EltwiseDotModAVX512(result, operand1, operand2, operand3, operand4, n,
+  //                         modulus);
+  //     return;
+  //   }
+  // #endif
 
   HEXL_VLOG(3, "Calling EltwiseDotModNative");
   EltwiseDotModNative(result, operand1, operand2, operand3, operand4, n,
