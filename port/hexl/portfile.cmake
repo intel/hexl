48ecb98b14ad9aa9af8583e3c199620ced7a48a0
GelilaSeifu <gelila.seifu@intel.com>
2021-07-20 08:19:32 -0700

    remove REF, SHA512, update readme

--- port/hexl/portfile.cmake
+++ port/hexl/portfile.cmake
@@ -4,8 +4,6 @@ vcpkg_fail_port_install(ON_ARCH "x86" "arm" "arm64")
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO intel/hexl
-    REF 3630e81528a676169b1b1fd199ede2f5d79d63eb
-    SHA512 f3e961b08bb161c67b0cdaf06a73bf026710dff5d8b17225e72464189aabbeb387a24c96c21c96f6a4ab60f138802cbab2a81bbe57959e63f5758596d310b122
     HEAD_REF 1.2.0
 )
 
