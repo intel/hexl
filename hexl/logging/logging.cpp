4676bbf3a31602e21f180a44b7f0514804d13240
Fabian Boemer <fabian.boemer@intel.com>
2021-08-12 11:17:47 -0700

    Fboemer/windows debug (#44)
    * Add Windows Debug build to CI, removing gflags

--- hexl/logging/logging.cpp
+++ hexl/logging/logging.cpp
@@ -4,26 +4,5 @@
 #include "hexl/logging/logging.hpp"
 
 #ifdef HEXL_DEBUG
-
-#include <gflags/gflags.h>
-
 INITIALIZE_EASYLOGGINGPP
-
-DEFINE_int32(v, 0,
-             "enable verbose (DEBUG) logging. Increasing verbosity from 1 to 5 "
-             "(maximum debugging)");
-
-el::Configurations LogConfigurationFromFlags() {
-  el::Configurations conf;
-  conf.setToDefault();
-  conf.set(el::Level::Global, el::ConfigurationType::ToFile, "false");
-
-  if (FLAGS_v) {
-    el::Loggers::setVerboseLevel(el::base::type::VerboseLevel(FLAGS_v));
-  } else {
-    conf.set(el::Level::Debug, el::ConfigurationType::Enabled, "false");
-  }
-  return conf;
-}
-
 #endif
