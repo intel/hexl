db73995480e1cf5d0f6cd91b6e861ec93c8d669e
Fabian Boemer <fabian.boemer@intel.com>
2021-08-20 13:06:24 -0700

    BM_FwdNTTNativeRadix4/1024 to 9.48us

--- hexl/ntt/ntt-radix-4.cpp
+++ hexl/ntt/ntt-radix-4.cpp
@@ -60,7 +60,7 @@ void ForwardTransformToBitReverseRadix4(
 
     uint64_t* X = operand;
     uint64_t* Y = X + t;
-    HEXL_LOOP_UNROLL_4
+    HEXL_LOOP_UNROLL_8
     for (size_t j = 0; j < t; j++) {
       FwdButterfly(X++, Y++, W, W_precon, modulus, twice_modulus);
     }
@@ -117,10 +117,10 @@ void ForwardTransformToBitReverseRadix4(
       }
       case 1: {
         for (size_t i = 0; i < m; i++) {
-          uint64_t X0_ind = 4 * i;
-          uint64_t X1_ind = X0_ind + gap;
-          uint64_t X2_ind = X0_ind + 2 * gap;
-          uint64_t X3_ind = X0_ind + 3 * gap;
+          uint64_t* X0 = operand + 4 * i;
+          uint64_t* X1 = X0 + gap;
+          uint64_t* X2 = X0 + 2 * gap;
+          uint64_t* X3 = X0 + 3 * gap;
 
           uint64_t W1_ind = m + i;
           uint64_t W2_ind = 2 * W1_ind;
@@ -134,11 +134,6 @@ void ForwardTransformToBitReverseRadix4(
           const uint64_t W2_precon = precon_root_of_unity_powers[W2_ind];
           const uint64_t W3_precon = precon_root_of_unity_powers[W3_ind];
 
-          uint64_t* X0 = &operand[X0_ind];
-          uint64_t* X1 = &operand[X1_ind];
-          uint64_t* X2 = &operand[X2_ind];
-          uint64_t* X3 = &operand[X3_ind];
-
           FwdButterflyRadix4(X0, X1, X2, X3, W1, W1_precon, W2, W2_precon, W3,
                              W3_precon, modulus, twice_modulus,
                              four_times_modulus);
@@ -147,30 +142,25 @@ void ForwardTransformToBitReverseRadix4(
       }
       default: {
         for (size_t i = 0; i < m; i++) {
-          HEXL_LOOP_UNROLL_4
+          uint64_t* X0 = &operand[4 * i * t];
+          uint64_t* X1 = &operand[4 * i * t + gap];
+          uint64_t* X2 = &operand[4 * i * t + 2 * gap];
+          uint64_t* X3 = &operand[4 * i * t + 3 * gap];
+
+          uint64_t W1_ind = m + i;
+          uint64_t W2_ind = 2 * W1_ind;
+          uint64_t W3_ind = 2 * W1_ind + 1;
+
+          const uint64_t W1 = root_of_unity_powers[W1_ind];
+          const uint64_t W2 = root_of_unity_powers[W2_ind];
+          const uint64_t W3 = root_of_unity_powers[W3_ind];
+
+          const uint64_t W1_precon = precon_root_of_unity_powers[W1_ind];
+          const uint64_t W2_precon = precon_root_of_unity_powers[W2_ind];
+          const uint64_t W3_precon = precon_root_of_unity_powers[W3_ind];
+
+          HEXL_LOOP_UNROLL_8
           for (size_t j = 0; j < t; j += 16) {
-            uint64_t X0_ind = j + 4 * i * t;
-            uint64_t X1_ind = X0_ind + gap;
-            uint64_t X2_ind = X0_ind + 2 * gap;
-            uint64_t X3_ind = X0_ind + 3 * gap;
-
-            uint64_t W1_ind = m + i;
-            uint64_t W2_ind = 2 * W1_ind;
-            uint64_t W3_ind = 2 * W1_ind + 1;
-
-            const uint64_t W1 = root_of_unity_powers[W1_ind];
-            const uint64_t W2 = root_of_unity_powers[W2_ind];
-            const uint64_t W3 = root_of_unity_powers[W3_ind];
-
-            const uint64_t W1_precon = precon_root_of_unity_powers[W1_ind];
-            const uint64_t W2_precon = precon_root_of_unity_powers[W2_ind];
-            const uint64_t W3_precon = precon_root_of_unity_powers[W3_ind];
-
-            uint64_t* X0 = &operand[X0_ind];
-            uint64_t* X1 = &operand[X1_ind];
-            uint64_t* X2 = &operand[X2_ind];
-            uint64_t* X3 = &operand[X3_ind];
-
             FwdButterflyRadix4(X0++, X1++, X2++, X3++, W1, W1_precon, W2,
                                W2_precon, W3, W3_precon, modulus, twice_modulus,
                                four_times_modulus);
