4f5dba21b15ceb305fe11609beb2a6aea2f32a10
Fabian Boemer <fabian.boemer@intel.com>
2021-10-22 11:18:11 -0700

    reproduce error

--- test/test-eltwise-cmp-sub-mod-avx512.cpp
+++ test/test-eltwise-cmp-sub-mod-avx512.cpp
@@ -57,6 +57,44 @@ TEST(EltwiseCmpSubMod, AVX512) {
     }
   }
 }
+
+TEST(EltwiseCmpSubMod, PALISADE) {
+  if (!has_avx512dq) {
+    GTEST_SKIP();
+  }
+
+  uint64_t length = 1024;
+
+  // uint64_t modulus = GeneratePrimes(1, bits, true, 1024)[0];
+
+  uint64_t modulus = 1125896819525633;
+
+  for (size_t trial = 0; trial < 200; ++trial) {
+    auto op1 = GenerateInsecureUniformRandomValues(length, 1106601337915084531,
+                                                   modulus);
+
+    uint64_t bound = 576460751967876096;
+    // Ensure diff != 0
+    uint64_t diff = 3160741504001;
+
+    auto op1a = op1;
+    auto op1b = op1;
+    std::vector<uint64_t> op1_out(op1.size(), 0);
+    std::vector<uint64_t> op1a_out(op1.size(), 0);
+    std::vector<uint64_t> op1b_out(op1.size(), 0);
+
+    EltwiseCmpSubMod(op1_out.data(), op1.data(), op1.size(), modulus,
+                     intel::hexl::CMPINT::NLE, bound, diff);
+    EltwiseCmpSubModNative(op1a_out.data(), op1a.data(), op1a.size(), modulus,
+                           intel::hexl::CMPINT::NLE, bound, diff);
+    EltwiseCmpSubModAVX512(op1b_out.data(), op1b.data(), op1b.size(), modulus,
+                           intel::hexl::CMPINT::NLE, bound, diff);
+
+    ASSERT_EQ(op1_out, op1a_out);
+    ASSERT_EQ(op1_out, op1b_out);
+  }
+}
+
 #endif
 }  // namespace hexl
 }  // namespace intel
