cb0c94f61f39a6845ed927e4cb4cfdb178b95d52
Jose Martin Rojas Chaves <jose.rojas.chaves@intel.com>
2022-06-30 15:03:22 -0700

    Adding doxygen params

--- hexl/include/hexl/experimental/fft-like/fwd-fft-like-avx512.hpp
+++ hexl/include/hexl/experimental/fft-like/fwd-fft-like-avx512.hpp
@@ -21,6 +21,15 @@ namespace hexl {
 /// @param[in] n Size of the transform, i.e. the polynomial degree. Must be a
 /// power of two.
 /// @param[in] scale Scale applied to output values
+/// @param[in] recursion_depth Depth of recursive call
+/// @param[in] recursion_half Helper for indexing roots of unity
+/// @details The implementation is recursive. The base case is a breadth-first
+/// FFT like, where all the butterflies in a given stage are processed before 
+/// any butterflies in the next stage. The base case is small enough to fit in
+/// the smallest cache. Larger FFTs are processed recursively in a depth-first
+/// manner, such that an entire subtransform is completed before moving to the
+/// next subtransform. This reduces the number of cache misses, improving
+/// performance on larger transform sizes.
 void Forward_FFTLike_ToBitReverseAVX512(
     double* result_cmplx_intrlvd, const double* operand_cmplx_intrlvd,
     const double* roots_of_unity_cmplx_intrlvd, const uint64_t n,
